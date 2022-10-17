18fd416df7b875ce758c2fe4011b432cf0d8ee11
briantkho <briantkho@gmail.com>
2022-10-16 20:04:31 -0700

    Test Coverage Fixes

--- src/test/model/PasswordManagerTest.java
+++ src/test/model/PasswordManagerTest.java
@@ -57,4 +57,41 @@ public class PasswordManagerTest {
         expectedOuput.put("account2", expectedList);
         assertEquals(expectedOuput, password.getAllDetails());
     }
+
+    @Test
+    public void getDetailTest() {
+        password.addDetailEntry("inputPassword", "inputEmail", "account1");
+
+        List<String> expectedOutput = new ArrayList<>();
+
+        expectedOutput.add("Account: account1");
+        expectedOutput.add("Email: inputEmail");
+        expectedOutput.add("Password: inputPassword");
+
+        assertEquals(expectedOutput, password.getDetail("account1"));
+        assertEquals(1, password.getAllDetails().size());
+    }
+
+    @Test
+    public void getAllDetailsTest() {
+        password.addDetailEntry("inputPassword", "inputEmail", "account1");
+        password.addDetailEntry("3k(&jJK<", "email2", "account2");
+
+        HashMap<String, List<String>> expectedOutput = new HashMap<>();
+
+        List<String> expectedList1 = new ArrayList<>();
+        expectedList1.add("Account: account1");
+        expectedList1.add("Email: inputEmail");
+        expectedList1.add("Password: inputPassword");
+
+        List<String> expectedList2 = new ArrayList<>();
+        expectedList2.add("Account: account2");
+        expectedList2.add("Email: email2");
+        expectedList2.add("Password: 3k(&jJK<");
+
+        expectedOutput.put("account1", expectedList1);
+        expectedOutput.put("account2", expectedList2);
+
+        assertEquals(expectedOutput, password.getAllDetails());
+    }
 }
