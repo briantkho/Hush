2eb86f4a86281466a17d51cadb39972aab44e427
briantkho <briantkho@gmail.com>
2022-11-18 02:54:26 -0800

    Fixed code coverage

--- src/main/exceptions/InvalidFieldsException.java
+++ src/main/exceptions/InvalidFieldsException.java
@@ -1,10 +1,6 @@
 package exceptions;
 
 public class InvalidFieldsException extends Exception {
-    protected InvalidFieldsException() {
-        super("Fields are Invalid!");
-    }
-
     public InvalidFieldsException(String message) {
         super(message);
     }
